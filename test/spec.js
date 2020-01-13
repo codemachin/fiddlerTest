ae1f04f5bdd505ed6141d52f02ad95d92c7faff9
shashank3008 <58253527+shashank3008@users.noreply.github.com>
2020-01-13 12:48:17 +0530

    automation code for MAC

--- test/spec.js
+++ test/spec.js
@@ -7,13 +7,16 @@ var chai = require('chai')
 var chaiAsPromised = require('chai-as-promised')
 chai.use(chaiAsPromised);
 chai.should()
-
+var menuAdd = require('spectron-menu-addon')
+console.log(menuAdd, typeof(menuAdd))
+menuAddon = new menuAdd.SpectronMenuAddon()
 
 var app = new Application({
   path: '/Applications/Progress Telerik Fiddler.app/Contents/MacOS/Progress Telerik Fiddler'
 })
 
 describe('Application launch', function () {
+  this.timeout(30000)
   
     before(function () {
       this.app = new Application({
@@ -24,21 +27,6 @@ describe('Application launch', function () {
       })
       return this.app.start()
     })
-  
-    // afterEach(function () {
-    //   if (this.app && this.app.isRunning()) {
-    //     return this.app.stop()
-    //   }
-    // })
-  
-    // it('shows an initial window', function () {
-    //   return this.app.client.getWindowCount().then(function (count) {
-    //     console.log("-========",count)
-    //     assert.equal(count, 1)
-    //     // Please note that getWindowCount() will return 2 if `dev tools` are opened.
-    //     assert.equal(count, 2)
-    //   })
-    // })
 
     it('shows an second window',  function () {
       return this.app.client.waitUntilWindowLoaded().getWindowCount().then(function (count) {
@@ -57,38 +45,21 @@ describe('Application launch', function () {
             done(error);
         }
     }
-    // it('should take less than 5000ms as well', function(done){
-    //     this.timeout(5000)
-    //     setTimeout(done, 4500);
-    // })
-    //it('try to open 2nd', function(done){
-       // this.timeout(20000)
-        // return this.app.client.waitUntilWindowLoaded().getWindowCount().then(function (count) {
-        //     console.log("@=======@",count)
-        //     done()
-        
-        // })
-        // Get the window's title
-        
-    //     it('has the correct title', async function(){
-    //         const title = await this.app.client.windowByIndex(0);
-    //         console.log(title)
-        
-    // })
+    
     it('shows an initial window', function (done) {
-      this.timeout(25000)
+      this.timeout(35000)
       this.app.client
       .getWindowCount().should.eventually.equal(1).then(()=>{
         setTimeout( () =>{
           this.app.client.windowByIndex(0)
           
           .getTitle().then((title)=>{
-            console.log("::::",title)
+            console.log("*******",title)
             this.app.client.click('button').then((r)=>{
               console.log(r)
               this.app.client.$$('a').then((r)=>{
                 this.app.client.getText('button').then((text)=>{
-                  console.log("el",text)
+                  console.log("Get Text:",text)
                   done()
 
                 })
@@ -98,48 +69,72 @@ describe('Application launch', function () {
             console.log(e)
           })
         }, 10000);
-
         
       })
     })
-        
-        
-        // const p = await this.app.client.windowByIndex(0)
-        // // await this.app.client.waitUntilWindowLoaded()
-
-        // const swin=await this.app.client.getTitle()
-        // if (swin!='Progress Telerik Fiddler'){
-        //     await x (10000)
-        //     await this.app.client.windowByIndex(1)
-        //     let res = await this.app.client.getTitle()
-        //     console.log(res)
-        // }
-  
-    // it('should give more info',  async function() {
-    //     this.timeout(5000);
-    //     let c = await this.app.client.waitUntilWindowLoaded().getWindowCount()
-    //     console.log("++++",c)
-    
-    //     // setTimeout(function () {
-    //     //     this.app.client.waitUntilWindowLoaded().getWindowCount().then(function (count) {
-    //     //         console.log("++++++++",count)
-    //     //     })
-            
-    
-    //     // }, 5000);
-    //  });
-})
-
-function x(){
-    return new Promise((resolve)=>{
-        setTimeout(()=>{
-            resolve()
-        },10000)
+//This will click on capture button.
+    it('next Click for capture button', function(done){
+      this.timeout(15000)
+        //console.log("Capture Button",title)
+        this.app.client.click('.capture-title').then((r)=>{
+          console.log(r)
+          setTimeout( async() =>{
+            //await Sleep(5000)
+            await this.app.client.click('button')
+            done()
+          },7000)
+        })
     })
-}
+    //This will click on Stream button.
+    it('This is for stream Button', function(done){
+      this.timeout(17000)
+        //console.log("Stream Button",title)
+        this.app.client.click('.content=Stream').then((r)=>{
+          console.log(r)
+          setTimeout( async() =>{
+            //await Sleep(5000)
+            await this.app.client.click('button')
+            done()
+          },9000)
+
+          
+   })
+        })
+         it('For Decode Button', function(done){
+          this.timeout(19000)
+           this.app.client.click('.content=Decode').then((r)=>{
+              console.log(r)
+             setTimeout( async() =>{
+              //await Sleep(5000)
+               await this.app.client.click('button')
+                done()
+              },11000)
+
+            })        
+          })
+
+          it('For Capture button', function(done){
+            this.timeout(21000)
+
+            menuAddon.createApplication({ args: ['/Applications/Progress Telerik Fiddler.app/Contents/MacOS/Progress Telerik Fiddler'], path: electronPath.toString() })
+ 
+            menuAddon.clickMenu('File'); // 'Config' Menu click
+            // menuAddon.clickMenu('File', 'Save'); // File->Save MenuItem click
+            // await menuAddon.isItemEnabled('File', 'Reset') // Verify if MenuItem File->Reset is enabled
+
+            //  this.app.client.click('.button-wrapper').then((r)=>{
+            //        console.log(r)
+            //      setTimeout( async() =>{
+            //        await this.app.client.click('button')
+            //        done()
+            //      },13000)
+  
+            //    }).catch((e)=>{
+            //      console.log(">>>>>>>>>>",e)
+            //    })        
+            
+          })
 
 
 
-
-
-
+})
